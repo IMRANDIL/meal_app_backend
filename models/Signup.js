3422083c5d570bca967691b5fd6767df9fbe5311
IMRANDIL <aliimranadil2@gmail.com>
2022-03-01 17:46:15 +0530

    yh

--- models/Signup.js
+++ models/Signup.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     userName: {
         type: String,
@@ -21,6 +21,27 @@ const userSchema = new mongoose.Schema({
 
     }
 })
+
+
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, 12, function (err, hash) {
+        if (err) return next(err);
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+    });
+
+});
+
+
 
 
 module.exports = mongoose.model('User', userSchema)
