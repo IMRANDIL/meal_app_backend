3fc71fd0175de430923116af37132494328c44b0
IMRANDIL <aliimranadil2@gmail.com>
2022-03-03 20:12:38 +0530

    yh

--- controllers/RemoveFav.js
+++ controllers/RemoveFav.js
@@ -3,6 +3,23 @@
 
 const postRemoveFav = async (req, res, next) => {
 
+    try {
+
+        const { mealId } = req.body;
+        const user = req.user;
+
+        user.favourites = user.favourites.filter((id) => id !== mealId)
+        await user.save();
+        res.status(200).send(user);
+
+
+        next()
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 
